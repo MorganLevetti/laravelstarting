0c49aed561f4a43c879255c556ff8bafc9ba3111
MorganLevetti <113036290+MorganLevetti@users.noreply.github.com>
2023-05-09 16:23:04 +0200

    init controller

--- routes/web.php
+++ routes/web.php
@@ -2,39 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('login', '@login'); 
-Route::post('register', '@register');
-Route::post('logout', '@logout');
-Route::post('refresh', '@refresh');
-Route::post('token', '@token');
+Route::post('login', 'ApiController@login'); 
+Route::post('register', 'ApiController@register');
+Route::post('logout', 'ApiController@logout');
+Route::post('refresh', 'ApiController@refresh');
+Route::post('token', 'ApiController@token');
 
 // BROWSE
-Route::get('/{datatype}', '@browse');
+Route::get('/{datatype}', 'ApiController@browse');
 
 // READ
-Route::get('/{datatype}/{id}', '@read');
+Route::get('/{datatype}/{id}', 'ApiController@read');
 
 // EDIT
-Route::put('/{datatype}/{id}', '@edit');
+Route::put('/{datatype}/{id}', 'ApiController@edit');
 
 // ADD
-Route::post('/{datatype}', '@add');
+Route::post('/{datatype}', 'ApiController@add');
 
 // DELETE
-Route::delete('/{datatype}/{id}', '@delete');
+Route::delete('/{datatype}/{id}', 'ApiController@delete');
 
